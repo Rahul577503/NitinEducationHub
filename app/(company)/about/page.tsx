f6028a8e7f4821c3373e3cea167e28a0688616e3
Rahul577503 <rahulmaurya109626@gmail.com>
2024-03-02 11:19:39 +0530

    Update page.tsx

--- app/(company)/about/page.tsx
+++ app/(company)/about/page.tsx
@@ -1,42 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
-'use client'
 import Head from "next/head";
 import Image from "next/legacy/image";
 import ContactUs from "@/components/Contact";
-import MyImage from "@/public/ProfileImage.svg";
+import ProfileImage from "@/public/ProfileImage.svg";
 
-const ProfileImage =
-  "https://img.freepik.com/free-photo/portrait-handsome-man_23-2150770959.jpg?size=626&ext=jpg&ga=GA1.1.706000927.1707051425&semt=sph";
-
-const AboutPage: React.FC = () => {
+const AboutPage = () => {
   return (
-    <div className="min-h-screen py-[120px] flex flex-col justify-center items-center px-4 lg:p-0">
-      <div className="max-w-[800px] flex flex-col justify-center items-center ">
+    <div className="min-h-screen py-12 flex flex-col justify-center items-center px-4 lg:p-0 bg-gray-100">
+      <div className="max-w-3xl bg-white p-8 rounded-lg shadow-md text-center">
         <Head>
           <title>About Us</title>
         </Head>
-        <Image
-          src={MyImage}
-          height={250}
-          width={250}
-          alt="Profile"
-          className="rounded-full h-40 w-40  mb-4"
-        />
-        <h1 className="text-3xl font-bold mb-2">Nitin Maurya</h1>
-        <p className="text-gray-600 text-2xl mb-3 text-center">Computer Teacher & Software Engineer</p>
-        <div className="">
-          <p className="text-center">
-            Hey everyone, I'm Nitin Maurya, a computer teacher! 💻 Teaching
-            computers?That's my forte! Whether it's explaining concepts ,
-            guiding through coding exercises, or answering tech questions, I've
-            got you covered. Need help understanding computers or software? I'm
-            here to make it easy for you. Let's dive into the world of
-            technology together! 🚀
-          </p>
+        <div className="relative rounded-full overflow-hidden h-36 w-36 mx-auto mb-4">
+          <Image
+            src={ProfileImage}
+            alt="Profile"
+            height={144}
+            width={144}
+            className="rounded-full"
+          />
         </div>
-      </div>
-      <div className="flex flex-col ">
-        <div className="p-2">
+        <h1 className="text-3xl font-bold mb-2">Nitin Maurya</h1>
+        <p className="text-gray-600 text-lg mb-4">Computer Teacher & Software Engineer</p>
+        <p className="text-lg text-gray-800 mb-8">
+          Hey everyone, I'm Nitin Maurya, a computer teacher! 💻 Teaching
+          computers? That's my forte! Whether it's explaining concepts,
+          guiding through coding exercises, or answering tech questions, I've
+          got you covered. Need help understanding computers or software? I'm
+          here to make it easy for you. Let's dive into the world of
+          technology together! 🚀
+        </p>
+        <div className="mt-8">
           <ContactUs />
         </div>
       </div>
