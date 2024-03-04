f9de0d0adea4722a8f63444ae9085e5c0935f84f
Rahul577503 <rahulmaurya109626@gmail.com>
2024-03-04 23:58:39 +0530

    Update Topper.tsx

--- components/Topper.tsx
+++ components/Topper.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { TfiCup } from "react-icons/tfi";
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 const CollageImage =
   "https://img.freepik.com/free-photo/vibrant-school-backpack-sitting-classroom_157027-4117.jpg?size=626&ext=jpg&ga=GA1.1.706000927.1707051425&semt=sph";
 
 const Topper = () => {
   return (
-    <motion.div
-      className="flex justify-center items-center py-4"
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, delay: 0.3 }}
-    >
+    <div className="flex justify-center items-center py-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <motion.div 
-          className="p-8 text-gray-800 flex flex-col items-center bg-green-50 justify-center"
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-        >
+        <div className="p-8 text-gray-800 flex flex-col items-center bg-green-50 justify-center">
           <div className="bg-green-500 rounded-full p-4 w-20 h-20 flex items-center justify-center mb-4">
             <TfiCup className="text-white text-4xl mx-auto" />
           </div>
@@ -30,16 +20,11 @@ const Topper = () => {
             stories, and their journey towards success. Get inspired and
             motivated to reach for the stars with NitinEduHub top performers
           </p>
-        </motion.div>
+        </div>
 
-        <motion.div 
-          className="px-4"
-          initial={{ opacity: 0, x: 20 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.5 }}
-        >
-          <div className="bg-green-50 rounded-lg shadow-md border border-gren-100 p-4 min-w-full overflow-hidden">
-            <div className="mb-4 mx-auto relative">
+        <div className="px-4">
+          <div className="bg-green-50 rounded-lg shadow-md border border-green-100 p-4 overflow-hidden">
+            <div className="mb-4 mx-auto">
               <Image
                 src={CollageImage}
                 alt="collage photos"
@@ -77,9 +62,9 @@ const Topper = () => {
               </tbody>
             </table>
           </div>
-        </motion.div>
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
