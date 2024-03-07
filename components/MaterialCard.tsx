5801dcf57a21788d94360010d3f68bf47beb8f8f
Rahul577503 <rahulmaurya109626@gmail.com>
2024-03-07 17:01:18 +0530

    Update MaterialCard.tsx

--- components/MaterialCard.tsx
+++ components/MaterialCard.tsx
@@ -15,7 +15,7 @@ const MaterialCard: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center lg:flex-row flex-col gap-4 p-4">
+    <div className="flex justify-center lg:flex-row md:flex-row flex-col gap-4 p-4">
       <div className="w-full md:w-1/2">
         <div className="max-w-full bg-gray-900 hover:ring-4 ring-yellow-400 rounded-2xl shadow-lg p-4">
           <h2 className="text-white text-xl font-bold mb-4 text-center">Study Material</h2>
